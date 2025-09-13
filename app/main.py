3a720487432fe7fd13eabd85ddbaebc9b770e041
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-09-13 21:27:42 +0200

    Resolved issues

--- app/main.py
+++ app/main.py
@@ -8,29 +8,26 @@ class Person:
     ) -> None:
         self.name = name
         self.age = age
-        self.people[name] = self
+        Person.people[name] = self
 
 
-def create_person_list(people: list[dict]) -> list:
-    person_list = []
+def create_person_list(
+    people: list[dict]
+) -> list[Person]:
+    person_list = [
+        Person(person["name"], person["age"])
+        for person in people
+    ]
 
-    for person in people:
-        new_person = Person(
-            person["name"],
-            person["age"]
-        )
+    for person_index in range(len(people)):
+        person = people[person_index]
 
-        if "wife" in person and person["wife"] is not None:
-            new_person.wife = person["wife"]
-        elif "husband" in person and person["husband"] is not None:
-            new_person.husband = person["husband"]
+        wife_name = person.get("wife")
+        husband_name = person.get("husband")
 
-        person_list.append(new_person)
-
-    for person in person_list:
-        if hasattr(person, "wife"):
-            person.wife = Person.people[person.wife]
-        if hasattr(person, "husband"):
-            person.husband = Person.people[person.husband]
+        if wife_name is not None:
+            person_list[person_index].wife = Person.people[wife_name]
+        elif husband_name is not None:
+            person_list[person_index].husband = Person.people[husband_name]
 
     return person_list
